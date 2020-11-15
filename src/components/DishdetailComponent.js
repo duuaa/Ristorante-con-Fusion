17430539377aba72ae66222a2d62bcc88bcb84bb
Duaa mohammed <Duaa.mohhammed@gmail.com>
2020-11-15 17:20:33 +0200

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Modal, ModalHeader,ModalBody,Input,Form,FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
 
 
@@ -23,8 +23,11 @@ class CommentForm extends Component{
       });
   }
   handleSubmit(values){
-      console.log("Current State is "+ JSON.stringify(values));
-      alert("Current State is "+ JSON.stringify(values));
+     // console.log("Current State is "+ JSON.stringify(values));
+      this.toggleModal();
+      this.props.addComment(this.props.dishId,values.rating, values.author,values.comment);
+    //   console.log("Current State is "+ JSON.stringify(values));
+     //alert("Current State is "+ JSON.stringify(values));
       //this prevents from going to another page
       //event.preventDefault();
   }
@@ -91,7 +94,8 @@ class CommentForm extends Component{
 }
 
 
-function RenderComments({ comments }) {
+
+function RenderComments({ comments ,addComment ,dishId}) {
 
     const com = comments.map((commente) => {
         return (
@@ -116,7 +120,7 @@ function RenderComments({ comments }) {
                 <h4> Comment </h4> {com}
             </div>
             <div>
-             <CommentForm/>
+             <CommentForm dishId={dishId} addComment={addComment}/>
              </div></div>
     );
 }
@@ -137,7 +141,25 @@ function RenderDish({ dish }) {
 }
 
 const Details = (props) => {
-    if (props.dish != null) {
+    if(props.isLoading){
+        return (
+            <div className ="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+
+    }else if(props.errMess){
+        return (
+            <div className ="container">
+                <div className="row">
+                     <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
@@ -156,7 +178,9 @@ const Details = (props) => {
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
 
                     </div>
                 </div>
@@ -171,3 +195,6 @@ const Details = (props) => {
 }
 
 export default Details;
+
+
+
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,49 +6,69 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import {Switch ,Route ,Redirect ,withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
-const mapStateToProps= state=>{
-  return{
-    dishes: state.dishes
-    ,comments : state.comments,
-    promotions : state.promotions,
-    leaders : state.leaders
+
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
   };
 }
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes()) }
+});
+
 class Main extends Component {
 
+  constructor(props) {
+    super(props);
+
+  }
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
 
   render() {
     const HomePage = () => {
-      return(
-          <Home 
-              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-          />
+      return (
+        <Home
+          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishErrMess={this.props.dishes.errMess}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
-    
-    const DishWithId = ({match}) => {
-      return(
-          <Details dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+
+    const DishWithId = ({ match }) => {
+      return (
+        <Details dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+          addComment={this.props.addComment} />
       );
-      
+
     };
 
-    
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/Home" component={HomePage} />
-          <Route exact path="/menu"component={()=><Menu dishes={this.props.dishes} /> } />
+          <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
-          <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders}/>} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
@@ -57,4 +77,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
